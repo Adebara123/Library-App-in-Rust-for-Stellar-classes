6a7008cb87a9a848618500e2e7f7ea125c687e6f
Adebara123 <adebaraayomide@gmail.com>
2025-03-26 19:10:57 +0100

    Chore: completed lib_users and started lib_boworring

--- lib_boworring/src/lib.rs
+++ lib_boworring/src/lib.rs
@@ -1,1 +1,30 @@
+use inventory::{Book, Inventory};
+use lib_users::UserManger;
 
+pub struct BorrowingService;
+
+impl  BorrowingService {
+    
+    pub fn new() -> Self {
+        BorrowingService
+    }
+
+    pub fn borrow_book (&self, inventory: &mut Inventory, user_manager: &mut UserManger, user_id: u32, book_id: u32) -> Result<(), String> {
+
+        // check if the book exist and is available 
+
+        let book = inventory.get_book(book_id).ok_or_else(|| "Book not found".to_string())?;
+
+        if !book.is_available {
+            return Err(String::from("Book not Available"));
+        }
+
+        // check if th user exists or hasn't borrowed more than 3 books 
+
+        // update the book availability 
+
+        // Add book to user's borrowed books 
+
+        todo!()
+    }
+}
--- lib_users/src/lib.rs
+++ lib_users/src/lib.rs
@@ -27,4 +27,35 @@ impl UserManger {
             user.borrowed_books.push(book_id);
         }
     }
+
+    pub fn return_book(&mut self, user_id: u32, book_id: u32) {
+        if let Some(user) = self.users.iter_mut().find(|u| u.id == user_id) {
+            
+            user.borrowed_books.retain(|&id| id != book_id);
+            
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_register_and_borrow_book() {
+        let mut user_manager = UserManger::new();
+
+        let user = User {
+            id: 1,
+            name: String::from("Victor"),
+            borrowed_books: Vec::new(),
+        };
+
+        user_manager.register_user(user);
+        assert_eq!(user_manager.get_user(1).unwrap().name, "Victor");
+
+        user_manager.borrow_book(1, 101);
+        assert!(user_manager.get_user(1).unwrap().borrowed_books.contains(&101));
+    }
 }
